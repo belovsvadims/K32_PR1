5a457eef3d56cd88870a4800bcf2b59a8a2b6748
Dmitrijs Ivanovs <dmitrijs.ivanovs8@gmail.com>
2024-04-09 21:57:35 +0300

    i love ai <3

--- EvaluationFunction.java
+++ EvaluationFunction.java
@@ -2,27 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EvaluationFunction {
-    private static final int MAX_DEPTH = 4; // koka dzilums
-    private static final int START_DEPTH = 1;
-    public static void main(String[] args) {
-        System.out.println(evaluateState(GameTree.createGameTree(9240, 1, 0, 0)));
-                // !parskatam! speles sakuma stavoklis (jo playerScore, computerScore un bank ir 0)
-
-        System.out.println("-------------");
-
-        System.out.println(leafNodes);      //!parskatam! izvada massivu ar strupceliem ar vertejumiem
-
-        System.out.println("-------------");
-
-        postOrder(evaluateState(GameTree.createGameTree(9240, 1, 0, 0)));
-                // //!parskatam! izvada koku ka sarakstu
-    }
-    static public void postOrder(GameTreeNode root) {   // pect tam izdzesim
-        if (root == null) return;                       // pect tam izdzesim
-        System.out.println(root);                       // pect tam izdzesim
-        postOrder(root.getDividingBy2());               // pect tam izdzesim
-        postOrder(root.getDividingBy3());               // pect tam izdzesim
-    }
+    private static final int MAX_DEPTH = GameTree.MAX_DEPTH; // koka dzilums
+    private static final int START_DEPTH = GameTree.START_DEPTH;
     private static List<GameTreeNode> leafNodes; // massivs ar stupceliem, kurus novertesim
     public static GameTreeNode evaluateState(GameTreeNode gameTreeNode) { // atgriež kokas virsoti (pašo pirmo) lai butu viegli stradat algoritmos
         leafNodes = new ArrayList<>(); // massiva deklaresana
@@ -30,6 +11,7 @@ public class EvaluationFunction {
         findLeafs(gameTreeNode, START_DEPTH); // izsaucam funkciju, lai atrast visus koka stupcelus
 
         for (GameTreeNode i : leafNodes) { // noverte strupcelus/speles..stavoklus (atnem no ComputerScore PlayerScore)
+            System.out.println(i.getEvaluationScore());
             if (i.getNumber() == 2 || i.getNumber() == 3) {
                 if (i.getEvaluationScore() == 0) {
                     i.setEvaluationScore(i.getComputerScore() - i.getPlayerScore() + i.getBank());
@@ -38,9 +20,9 @@ public class EvaluationFunction {
                 }
             } else {
                 if (i.getEvaluationScore() == 0) {
-                    i.setEvaluationScore(i.getComputerScore() - i.getPlayerScore());
-                } else {
                     i.setEvaluationScore(i.getPlayerScore() - i.getComputerScore());
+                } else {
+                    i.setEvaluationScore(i.getComputerScore() - i.getPlayerScore());
                 }
             }
         }
@@ -62,5 +44,4 @@ public class EvaluationFunction {
             findLeafs(gameTreeNode.getDividingBy3(), depth + 1);
         }
     }
-
 }
